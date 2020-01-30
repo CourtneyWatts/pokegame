8e22701fb71795a786424a95e01a70b8ed5355bc
Courtney-Watts <courtney@siteglide.com>
2020-01-30 16:13:06 +0000

    Updates

---
+++ precache-manifest.1459860563f69407ee0ccf213eee16d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbefc9c03c91f818a488dd58b9f6e9fb",
+    "url": "/pokegame/index.html"
+  },
+  {
+    "revision": "06d0f232c89a244f20cd",
+    "url": "/pokegame/static/css/main.b30de036.chunk.css"
+  },
+  {
+    "revision": "b69655b057e0ed94d3e1",
+    "url": "/pokegame/static/js/2.d4bb108a.chunk.js"
+  },
+  {
+    "revision": "c6f2a8cff5bda75ce9d9d2e5dad3a2a7",
+    "url": "/pokegame/static/js/2.d4bb108a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06d0f232c89a244f20cd",
+    "url": "/pokegame/static/js/main.fd7a166b.chunk.js"
+  },
+  {
+    "revision": "189681251cb37615f30c",
+    "url": "/pokegame/static/js/runtime-main.59e3871c.js"
+  },
+  {
+    "revision": "da0e97f88d7ce112c3e0ddb3d73a4594",
+    "url": "/pokegame/static/media/redo.da0e97f8.svg"
+  }
+]);
